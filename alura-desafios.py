a1b84b8aa63aaa3431c628ac4022a792c7d0b2f7
Gabriel Oliveira <gabrieldev.deoliveira@gmail.com>
2024-04-24 16:04:53 -0300

    Desafio-Criação-Class

--- alura-desafios.py
+++ alura-desafios.py
@@ -1,8 +1,16 @@
-numero = 10
+class Musica:
+    nome = ''
+    artista = ''
+    duracao = float
 
-def decrescente():
-    global numero
-    for numero in range(numero, 0, -1):
-        print(f'{numero}')
+musica_gospel = Musica()
+musica_gospel.nome = 'Quero Exaltar'
+musica_gospel.artista = 'Lorena Oliveira'
+musica_gospel.duracao = 3.35
 
-decrescente()
+musica_trap = Musica()
+musica_trap.nome = 'Diz ai Qual é o Plano?'
+musica_trap.artista = 'MC Ig'
+musica_trap.duracao = 11.43
+
+print(vars(musica_gospel), vars(musica_trap))
