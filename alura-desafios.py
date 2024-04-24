bc377ff78024c7d78c92e944028b1f2690ac1b5a
Gabriel Oliveira <gabrieldev.deoliveira@gmail.com>
2024-04-24 13:24:11 -0300

    Sexto-Desafio

--- alura-desafios.py
+++ alura-desafios.py
@@ -1,20 +1,6 @@
-def eixo():
-    x = float((input('Digite a coordenada x: ')))
-    y = float((input('Digite a coordenada y: ')))
-    # condicionais
-    primeiro_quadrante = x > 0 and y > 0
-    segundo_quadrante = x < 0 and y > 0
-    terceiro_quadrante = x < 0 and y < 0
-    quarto_quadrante = x > 0 and y < 0
+elementos =  ['Água', 'Fogo', 'Terra', 'Grama']
 
-    if (primeiro_quadrante):
-        print(f'O ponto está no primeiro quadrante.')
-    elif (segundo_quadrante):
-        print('O ponto está no segundo quadrante.')
-    elif (terceiro_quadrante):
-        print('O ponto está no terceiro quadrante.')
-    elif (quarto_quadrante):
-        print('O ponto está no quarto quadrante.')
-    else:
-        print('O ponto está sobre um eixo ou na origem.')
-eixo()
+def elementos_do_mundo():
+    for elemento in elementos:
+        print(f'Elemeno {elemento}')
+elementos_do_mundo()
