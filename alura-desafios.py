efd09b23a5c48661133ed76b68e1f0ed2dc96d3e
Gabriel Oliveira <gabrieldev.deoliveira@gmail.com>
2024-04-24 13:20:04 -0300

    Segundo-Desafio

---
+++ alura-desafios.py
@@ -0,0 +1,7 @@
+def numero():
+    solicite = int(input('Digite um número: '))
+    if (solicite %2 == 0):
+        print('Esse número é par')
+    else:
+        print('Esse número é impar')
+numero()
