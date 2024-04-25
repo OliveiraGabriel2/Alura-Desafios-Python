d43f0411a0adb7dbc1f8a211f7de6474a886a484
Gabriel Oliveira <gabrieldev.deoliveira@gmail.com>
2024-04-25 15:39:05 -0300

    Desafio-Class-Restaurante

--- alura-desafios.py
+++ alura-desafios.py
@@ -1,12 +1,13 @@
-class Carro():
-    def __init__(self, modelo, cor, ano):
-        self.modelo = modelo
-        self.cor = cor
-        self.ano = ano
+class Restaurante():
+    def __init__(self, nome, categoria, prato_principal, valor_entrada):
+        self.nome = nome
+        self.categoria = categoria
+        self.prato_principal = prato_principal
+        self.valor_entrada = valor_entrada
 
     def __str__(self):
-        return f'{self.modelo} | {self.cor} | {self.ano}'
+        return f'Nome: {self.nome} | Categoria: {self.categoria} | Prato-principal: {self.prato_principal} | Valor da entrada: R${self.valor_entrada} reais'
 
-carro_ferrari = Carro('Ferrari', 'Vermelha', '2024')
-print(carro_ferrari)
+restaurante_gourmet = Restaurante('Vitrini', 'Gourmet', 'Risoto de cogumelos selvagens com trufas', int('30'))
+print(restaurante_gourmet) 
 
